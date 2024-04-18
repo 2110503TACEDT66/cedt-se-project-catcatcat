592db1f85f26bb05947dbbd8d9ce526483ecd8dd
Pariodus <145102769+Pariodus@users.noreply.github.com>
2024-04-18 14:56:28 +0700

    myreservation component

--- frontend/src/components/MyReservation.tsx
+++ frontend/src/components/MyReservation.tsx
@@ -18,8 +18,48 @@ export default function MyReservation({ reservations, user }: { reservations: Re
         return 1;
     });
 
+    const removeConfirm = useRef<HTMLDivElement>(null)
+    const waitingRemove = useRef<HTMLDivElement>(null)
+
+    const [item, setItem] = useState<Reservation | null>(null)
+
+    const removeReservationButton = (item: Reservation) => {
+        if (removeConfirm.current) removeConfirm.current.classList.toggle('hidden')
+        setItem(item)
+    }
+
+    const removeReservation = () => {
+        if (item) {
+            if (removeConfirm.current) removeConfirm.current.classList.toggle('hidden')
+            if (waitingRemove.current) waitingRemove.current.classList.toggle('hidden')
+            deleteReservation(item._id, user.token)
+                .then(() => {
+                    router.refresh()
+                    if (waitingRemove.current) waitingRemove.current.classList.toggle('hidden')
+                })
+        }
+    }
+    const [selectedStatus, setSelectedStatus] = useState<string>('all');
+
+    const handleChangeStatus = (status: string) => {
+        setSelectedStatus(status);
+    };
+
     return (
         <div>
+            <div className=" flex justify-end px-5 mx-5 py-2 my-5 ">
+                <select
+                    className={`px-3 py-1 border rounded ${selectedStatus === 'pending' ? 'border-[#E39D48]' : selectedStatus === 'waiting' ? 'border-gray-500' : selectedStatus === 'success' ? 'border-teal-500' : selectedStatus === 'failed' ? 'border-red-400' : 'border-sky-900'}`}
+                    value={selectedStatus}
+                    onChange={(e) => handleChangeStatus(e.target.value)}
+                >
+                    <option value="all">All</option>
+                    <option value="pending">Pending</option>
+                    <option value="waiting">Waiting</option>
+                    <option value="success">Success</option>
+                    <option value="failed">Failed</option>
+                </select>
+            </div>
             {
 
                 sortedReservations.length !== 0 ?
@@ -66,6 +106,25 @@ export default function MyReservation({ reservations, user }: { reservations: Re
                         ))
                     : <div className="flex justify-center items-center text-center text-lg md:text-xl lg:text-2xl font-bold mt-[80px] mx-[50px]">You don't have any reservations</div>
             }
+            <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 hidden" ref={removeConfirm}>
+                <div className="bg-white p-8 rounded-lg shadow-md mx-4">
+                    <div className="text-2xl font-bold mb-4">Are you sure to remove this reservation?</div>
+                    <div className='space-x-4 flex justify-end'>
+                        <button className="mt-4 px-4 py-2 text-black rounded-lg border-2 border-black" onClick={(e) => { if (removeConfirm.current) removeConfirm.current.classList.toggle('hidden') }}>
+                            Close
+                        </button>
+                        <button className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg" onClick={removeReservation}>
+                            Remove
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 hidden" ref={waitingRemove}>
+                <div className="bg-white p-8 rounded-lg shadow-md mx-4 text-center">
+                    <div className="text-2xl font-bold mb-4">Removing the reservation...</div>
+                    <div>Please wait a moment</div>
+                </div>
+            </div>
         </div>
     )
 }
