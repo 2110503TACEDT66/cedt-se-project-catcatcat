14bcd68e23b237428f48fa2d684d3fad3293af41
Jirinmas Siripakornchai <naniko.study@gmail.com>
2024-04-27 13:49:32 +0700

    add premium role authorize

--- backend/routes/premiumtransactions.js
+++ backend/routes/premiumtransactions.js
@@ -5,7 +5,7 @@ const router = express.Router({mergeParams: true});
 
 const {protect, authorize} = require('../middleware/auth');
 
-router.route('/').get(protect, getPremiumTransactions).post(protect, authorize('admin', 'user'), addPremiumTransaction);
-router.route('/:id').get(protect, authorize('admin', 'user'), getPremiumTransaction).delete(protect, authorize('admin', 'user'), deletePremiumTransaction);
+router.route('/').get(protect, getPremiumTransactions).post(protect, authorize('admin', 'user', 'premium'), addPremiumTransaction);
+router.route('/:id').get(protect, authorize('admin', 'user', 'premium'), getPremiumTransaction).delete(protect, authorize('admin', 'user', 'premium'), deletePremiumTransaction);
 
 module.exports = router;
