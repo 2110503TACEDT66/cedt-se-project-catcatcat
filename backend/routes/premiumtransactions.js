ce96c72f9ce45d1a5c62539ed938a52ed7d3e36d
Jirinmas Siripakornchai <naniko.study@gmail.com>
2024-04-27 23:30:41 +0700

    swagger: premiumtransaction GET-POST

--- backend/routes/premiumtransactions.js
+++ backend/routes/premiumtransactions.js
@@ -9,3 +9,68 @@ router.route('/').get(protect, getPremiumTransactions).post(protect, authorize('
 router.route('/:id').get(protect, authorize('admin', 'user', 'premium'), getPremiumTransaction).delete(protect, authorize('admin', 'user', 'premium'), deletePremiumTransaction);
 
 module.exports = router;
+
+
+/**
+* @swagger
+* /premiumtransactions:
+*   get:
+*     summary: Returns the list of all Premium Transactions
+*     tags: [Premium Transactions]
+*     responses:
+*       200:
+*         description: The list of the Premium Transactions
+*         content:
+*           application/json:
+*             schema:
+*             type: array
+*             items:
+*               $ref: '#/components/schemas/PremiumTransaction'
+*/
+
+/**
+* @swagger
+* /premiumtransactions/{id}:
+*   get:
+*     summary: Get the Premium Transaction by id
+*     tags: [Premium Transactions]
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*         required: true
+*         description: The premium transaction id
+*     responses:
+*       200:
+*         description: The Premium Transaction description by id
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/PremiumTransaction'
+*       404:
+*         description: No Premium Transaction with the reservation id
+*/
+
+/**
+* @swagger
+* /premiumtransactions:
+*   post:
+*     summary: Create a new premium transaction
+*     tags: [Premium Transactions]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/PremiumTransaction'
+*     responses:
+*       201:
+*         description: The premiumtransaction was successfully created
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/PremiumTransaction'
+*       500:
+*         description: Cannot create premium transaction
+*/
